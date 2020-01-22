4810bed818cb77bf5ece64c4951f3ace3bf286cc
Duran-FerrerM <38099617+Duran-FerrerM@users.noreply.github.com>
2020-01-22 18:22:29 +0100

    Add files via upload

---
+++ SI1.R
@@ -0,0 +1,33 @@
+##
+## B-cell tumor classifier example
+##
+
+
+# download a file with the classifier, load it in R, and delete the file
+download.file("https://github.com/Duran-FerrerM/Pan-B-cell-methylome/raw/master/methy.classifier.RData", destfile = "methy.classifier.RData", method="libcurl")
+load("methy.classifier.RData")
+file.remove("methy.classifier.RData")
+
+# Description of the function methy.classifier()
+# This function predicts the entity/subtype of a tumor sample based on reduced CpG set published in Duran-Ferrer, M, 2019.
+
+# Output
+# List of tables (one for each predictor) with the estimated probabilities of the classes, and
+# a final table with the predicted entity/subtype for each sample. The output includes a raw svm prediction and a
+# suggested prediction that takes into account confusion among entities/subtypes. If only one predictor is applied (see arguments) the ouptut
+# only includes a table with the predictions of that specific predictor
+
+# Function arguments
+# data: data.frame or matrix with the methylation beta values and named rows(samples) and columns(cpgs)
+# which.predictor: character string that specifies if the full prediction (entity+subtype) or which one of the five predictors will be applied.
+# Possible values are "entity.subtype","entity","ALL","CLL","DLBCL" or "MCL". Defaults to "entity.subtype" (full prediction)
+# impute.missings: impute missing beta values? Default to FALSE. Use with caution, the effect of missings to the predictions
+# have not been extensively tested
+# export: export results to a .xlsx file? The file will be named "methy.classifier.results.xlsx" and stored in the working directory.
+
+# example
+
+methy.classifier(data=example)
+methy.classifier(data=example,which.predictor="MCL",export=TRUE)
+methy.classifier(data=example,which.predictor="entity.subtype",export=TRUE)
+
---
+++ SI2.R
@@ -0,0 +1,23 @@
+##
+## epiCMIT calculation example
+##
+
+
+# download a file necessary data load it in R, and delete the file
+download.file("https://github.com/Duran-FerrerM/Pan-B-cell-methylome/raw/master/epiCMIT.example.RData", destfile = "methy.classifier.RData", method="libcurl")
+load("methy.classifier.RData")
+file.remove("methy.classifier.RData")
+
+# Description of the function Estimate.epiCMIT()
+# This function calculates the epiCMIT, epiCMIT-hyper and epiCMIT-hypo in B-cell tumors as published in Duran-Ferrer, M, 2019.
+
+# Output
+# A data.frame with epiCMIT,epiCMIT-hyper and epiCMIT-hypo in all the samples
+
+
+# Function arguments
+#betas: DNA methylation matrix of samples to calculate the epiCMIT. CpGs on rows, samples on columns. Rownames of matrix should be names of CpGs. See betas.example.
+#epiCMIT.annot: Annotation necessary for calculation of epiCMIT. epiCMIT have been built with 450K aray data.
+#export: Whether to export or not results in the current directory. Default to FALSE
+
+Estimate.epiCMIT(betas = betas.example,epiCMIT.annot = epiCMIT.annot,export = T)
