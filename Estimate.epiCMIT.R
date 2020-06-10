af63fbca52cb4a066405c43e8838445f7bf264b3
Duran-FerrerM <38099617+Duran-FerrerM@users.noreply.github.com>
2020-06-10 12:59:52 +0200

    Update Estimate.epiCMIT.R

--- Estimate.epiCMIT.R
+++ Estimate.epiCMIT.R
@@ -5,9 +5,9 @@
 #version 2, last updated 10/06/2020
 
 # download a file necessary data load it in R, and delete the file
-download.file("https://github.com/Duran-FerrerM/Pan-B-cell-methylome/raw/master/epiCMIT.example.RData", destfile = "methy.classifier.RData", method="libcurl")
-load("methy.classifier.RData")
-file.remove("methy.classifier.RData")
+download.file("https://github.com/Duran-FerrerM/Pan-B-cell-methylome/raw/master/Estimate.epiCMIT.v.2.RData", destfile = "Estimate.epiCMIT.v.2.RData", method="libcurl")
+load("Estimate.epiCMIT.v.2.RData")
+file.remove("Estimate.epiCMIT.v.2.RData")
 
 # Description of the function Estimate.epiCMIT()
 # This function calculates the epiCMIT, epiCMIT-hyper and epiCMIT-hypo in B-cell tumors as published in Duran-Ferrer, M, 2020.
